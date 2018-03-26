6b558410390ad533a3c58f1c0d22d286f2416c40
张宇 <runinspring@gmail.com>
2018-03-26 11:16:29 +0800

    FB InstantGame v2.0

--- fbinstant.d.ts
+++ fbinstant.d.ts
@@ -1,32 +1,21 @@
 
+
 //Copyright (c) 2014-present, Egret Technology.
-//fbinstant.1.1.js
+//for fbinstant.2.0.js
 declare class FBInstant {
     /**
-     * 获取语言种类，例如:zh_CN en_US
+     * 获取用户的地域信息，例如:zh_CN en_US
      */
-    static locale: string;
+    static getLocale(): string;
     /**
      * 获取运行的平台信息，例如:iOS, android 和 web
      */
-    static platform: string;
-    /**
-     * 获取用户信息
-     */
-    static player: FBPlayer;
-    /**
-     * SDK 的版本号，例如: '1.1'
-     */
-    static sdkVersion: string;
-    /**
-     * 当前游戏的来源信息
-     */
-    static context: Context;
+    static getPlatform(): string;
     /**
      * SDK 初始化结束会返回一个 Promise 方法
      * 应当在其他 API 使用前调用
      */
-    static initializeAsync(): promise;
+    static initializeAsync(): Promise;
     /**
      * 通知平台资源加载的百分比
      * @param percentage 0-100
@@ -36,7 +25,20 @@ declare class FBInstant {
      * 游戏已完成加载资源，用户点击了开始游戏的按钮
      * 返回一个 Promise 方法
      */
-    static startGameAsync(): promise;
+    static startGameAsync(): Promise;
+    /**
+     * 获取用户信息
+     */
+    static player: FBPlayer;
+    /**
+     * 当前游戏的来源信息
+     */
+    static context: Context;
+    /**
+     * SDK 的版本号，例如: '2.0'
+     */
+    static getSDKVersion(): string;    
+    
     /**
      * 通知 Facebook 平台当前的分数
      * @param score 玩家在游戏里的分数
@@ -46,18 +48,18 @@ declare class FBInstant {
      * 显示平台统一的游戏结束画面
      * 当游戏重新开始的时候，返回一个 Promise 方法。
      */
-    static endGameAsync(): promise;
+    static endGameAsync(): Promise;
     /**
      * 进行截屏，用户以后可以分享给好友。
      */
-    static takeScreenshot(): void;
+    static takeScreenshotAsync(): Promise;
     /**
      * 发送分享给好友的截屏画面。
      * @param base64picture 把截图进行 base64 编码后的字符串
      */
-    static sendScreenshot(base64picture: string): void;
+    static sendScreenshotAsync(base64picture: string): Promise;
     /**
-     * 遇到错误中止游戏。只有当游戏进入不可恢复的状态时才被调用。
+     * 遇到错误中止游戏。只有当游戏进入不可恢复的状态时才可被调用。
      * @param e 错误信息
      */
     static abort(e: any): void;
@@ -66,26 +68,38 @@ interface FBPlayer{
     /**
      * 用户的唯一标识ID
      */
-    id: string;
+    getID(): string;
     /**
-     * 取回当前用户在平台储存的数据
+     * 获取用户在Facebook上的的名字，使用用户的语言种类显示
+     */
+    getName(): string;
+    /**
+     * 获取用户在Facebook上的头像的url，头像为正方形，最小尺寸为200x200
+     */
+    getPhoto(): string;
+    /**
+     * 取回在FB平台储存的当前用户的数据
      * @param keys 数据的 key 的数组
      */
-    getDataAsync(keys: string[]): promise;
+    getDataAsync(keys: string[]): Promise;
     /**
-     * 把当前用户的数据储存在平台上。
+     * 把当前用户的数据储存在FB平台上。
      * @param data 包含key-value的数据对象.
      */
-    setDataAsync(data :Object): promise;
+    setDataAsync(data :Object): Promise;
 }
 /**
  * 回调方法
  */
-interface promise {
+interface Promise {
     /**
-     * @param fuc 回调方法
+     * @param callback 成功后的回调方法
      */
-    then(fuc: Function): void;
+    then(callback: Function): Promise;
+    /**
+     * @param callback 捕获失败的回调方法
+     */
+    catch(callback: Function): Promise;
 }
 /**
  * 当前游戏的来源信息
@@ -94,5 +108,5 @@ interface Context{
     /**
      * 当前游戏来源的唯一id
      */
-    id: string;
+    getID(): string;
 }
